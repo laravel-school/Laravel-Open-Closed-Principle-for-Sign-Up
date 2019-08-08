6192a3e3e00a2f9b9f4e63d5e400d482e459a94d
tisuchi <tisuchi@gmail.com>
2019-08-08 20:42:05 +0600

    Update JoinController

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -1,10 +1,22 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Http\Repositories\JoinByEmail;
+use App\Http\Repositories\JoinRepository;
+use App\Http\Repositories\AuthRepository;
 use Illuminate\Http\Request;
 
 class JoinController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Put form validation
+
+        $authRepository = new JoinRepository(new JoinByEmail());
+
+        $authRepository->store($request);
+
+        return redirect()->route('join')->with('success', 'You have joined successfully.');
+    }
 }
