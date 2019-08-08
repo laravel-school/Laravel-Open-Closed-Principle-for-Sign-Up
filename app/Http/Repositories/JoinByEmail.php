0d341550ddd6037f2a8ae46a1fee5e49bed16b6f
tisuchi <tisuchi@gmail.com>
2019-08-08 20:47:30 +0600

    JoinByEmail update

--- app/Http/Repositories/JoinByEmail.php
+++ app/Http/Repositories/JoinByEmail.php
@@ -1,7 +1,34 @@
-<?php 
+<?php
 namespace App\Http\Repositories;
 
-class JoinByEmail
+use App\Http\Interfaces\JoinInterface;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
+
+class JoinByEmail implements JoinInterface
 {
-	
+    public function store(Request $request)
+    {
+        $name = $request->first_name . ' ' . $request->last_name;
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'name' => $name,
+            'slug' => Str::slug($name, '-'),
+            'is_public' => 1,
+            'usertype' => User::USERTYPE['regular'],
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'activate' => User::ACTIVATE['inactive'],
+            'flag' => 0,
+            'activationcode' => Str::random(32)
+        ]);
+
+        // Trigger an email to sign up user
+
+        return $user;
+    }
 }
